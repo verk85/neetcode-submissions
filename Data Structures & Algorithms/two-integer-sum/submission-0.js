179b7deded002f4054d974894f0edc03bd5762d5
Verk <jorge85.st@gmail.com>
2026-03-30 17:35:33 -0700

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    
+    // we need to store complemental values based on the nums and target
+    twoSum(nums, target) {
+        let complements = {};
+        for(let i = 0; i < nums.length; i++){
+            if(complements[nums[i]] !== undefined){
+                return [complements[nums[i]],i]
+            }
+            let complement = target - nums[i];
+            complements[complement] = i;
+        }
+        return [];
+    }
+}
