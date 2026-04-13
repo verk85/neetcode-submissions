d730c61555df9b994fd8b931248aea8334afeedb
Verk <jorge85.st@gmail.com>
2026-04-12 17:37:21 -0700

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let sNums = nums.sort((a,b)=> a - b)
+        let kMostRepeated = [];
+        let currentNum = sNums[0];
+        let currentNumCount = 0;
+        for(let i = 0;  i <= sNums.length; i++){
+            if(currentNum !== sNums[i]){
+                kMostRepeated.push([currentNum, currentNumCount])
+                currentNum = sNums[i];
+                currentNumCount = 1;
+                continue;
+            }
+            currentNumCount++;
+        }
+        console.log("kMostRepeated: ", kMostRepeated);
+        console.log("kMostRepeated: ", kMostRepeated[0][0]);
+        if (k===1){
+            return [kMostRepeated[0][0]];
+        }
+        let skMostRepeated = kMostRepeated.sort((a,b)=> b[1] - a[1])
+        console.log("skMostRepeated: ", skMostRepeated);
+        console.log("skMostRepeated: ", skMostRepeated[0][0]);
+        let result = [];
+        for(let i = 0; i < k; i++){
+            result.push(skMostRepeated[i][0])
+        }
+        return result;
+    }
+
+}
